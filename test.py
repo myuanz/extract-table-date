253d29633aa4a4eb389bbd376ce3248b3037231e
provefar <provefars@gmail.com>
2019-11-04 13:11:01 +0800

    add some

--- test.py
+++ test.py
@@ -2,10 +2,10 @@
 from spacy import displacy
 import zh_core_web_sm
 from bs4 import BeautifulSoup
+import re
+
+# %%
 nlp = zh_core_web_sm.load()
-
-
-
 # %%
 def print_nlp(doc):
     for token in doc:
@@ -36,11 +36,16 @@ def dpdd(elem):
             dpdd(i)
     else:
         text = iuli(elem)
-        print([ord(i) for i in text])
         if text:
-            print(text)
+            print_nlp(nlp(text))
             print('*'*10)
 
 dpdd(bs)
 print('end')
 # %%
+m = re.match(r"(?P<mon>\d+(?=月))", "10月2日")
+m.groupdict()
+
+# %%
+m = re.search(r"(?P<mon>\d+(?=月))(?P<day>\d+(?=日))+", "10月2日")
+m.groupdict()
